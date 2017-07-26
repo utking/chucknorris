668122fc903a73690e03798310fc37569fe5aacd
UtkinG <utking@mail.ru>
2017-07-26 20:02:05 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <chuck-norris></chuck-norris>
+  </div>
+</template>
+
+<script>
+import ChuckNorris from './components/ChuckNorris';
+
+export default {
+  name: 'app',
+  components: {
+    ChuckNorris,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ChuckNorris.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="chucknorris">
+    <h1>About Chuck Norris:</h1>
+    <h2>{{ quote }}</h2>
+    <br>
+
+    <a v-on:click="fetchNext" href="#">I want more!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'chucknorris',
+  data() {
+    return {
+      quote: 'Wait for it...',
+    };
+  },
+  created() {
+    this.fetchNext();
+  },
+  methods: {
+    fetchNext(e) {
+      if (e) {
+        e.preventDefault();
+      }
+      this.$http.get('https://api.chucknorris.io/jokes/random')
+      .then((resp) => {
+        this.quote = resp.data.value;
+      }, () => {
+        this.quote = 'Failed to load one :(';
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+a {
+  color: #AAA;
+  text-decoration: none;
+}
+a:hover {
+  color: #555;
+}
+</style>
